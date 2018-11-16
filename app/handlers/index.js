c06051e016cd5c26f82d2288cd0380b4d0be1f2b
Oleg <o.rybak@astoundcommerce.com>
2018-11-16 16:31:27 +0200

    logger

--- app/handlers/index.js
+++ app/handlers/index.js
@@ -1,14 +1,14 @@
 const bodyparser = require('koa-bodyparser');
-const logger = require('koa-logger');
+// const logger = require('koa-logger');
 const error = require('./error');
 const jwt = require('./jwt');
 const { IS_DEV } = require('../utils/env');
 
 module.exports = (app, appWs) => {
-  if (IS_DEV) {
-    app.use(logger());
-    appWs.use(logger());
-  }
+  // if (IS_DEV) {
+  //   app.use(logger());
+  //   appWs.use(logger());
+  // }
 
   app.use(error());
   app.use(bodyparser());
